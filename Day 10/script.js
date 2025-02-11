3e37104b9bfba62b2df0fdc0b07b7582a9e5f426
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-02-11 14:44:30 -0600

    Added a list for Company names as well.

--- Day 10/script.js
+++ Day 10/script.js
@@ -7,7 +7,22 @@ document.getElementById("fetchUsers").addEventListener("click", function(){
 
         data.forEach(user => {
             let li = document.createElement("li");
-            li.innerHTML= `<strong> ${user.name}</strong> - ${user.email}`;
+            li.innerHTML= `<strong> ${user.username}</strong>`;
+            userList.appendChild(li);
+        });
+    })
+    .catch(error => console.error("Error fetching data: ", error));
+});
+document.getElementById("fetchCompany").addEventListener("click", function(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response=>response.json())
+    .then(data=>{
+        let userList = document.getElementById("companyList");
+        userList.innerHTML="";
+
+        data.forEach(user => {
+            let li = document.createElement("li");
+            li.innerHTML= `<strong> ${user.company.name}</strong>`;
             userList.appendChild(li);
         });
     })
