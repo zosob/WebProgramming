2a6d2e46632abe7e2f0808da96f25f15ab69e9c8
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-03-03 13:22:08 -0600

    Mood Tracking Function Componenet added

---
+++ Day 12/mood-tracker/src/MoodTracker.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+export default function MoodTracker(){
+
+    const [mood, setMood] = useState("😊");
+
+    return(
+        <div style={{textAlign:"center", padding:"20px"}}>
+            <h1>Mood Tracker</h1>
+            <p>Current Mood: {mood}</p>
+            <button onClick={()=>setMood("😂")} style={{margin: "5px", padding : "10px", background: "lightgreen"}}> Very Happy </button>
+            <button onClick={()=>setMood("😒")} style={{margin: "5px", padding : "10px", background: "lightblue"}}> Unamused </button>
+            <button onClick={()=>setMood("😭")} style={{margin: "5px", padding : "10px", background: "lightcoral"}}> Upset </button>
+            <button onClick={()=>setMood("😁")} style={{margin: "5px", padding : "10px", background: "lightgray"}}> Beaming </button>
+        </div>
+    );
+}
